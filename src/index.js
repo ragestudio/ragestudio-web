b8b42e3699d04db1a4a49240064d3cd45ddfa78d
SrGooglo <srgooglo@ragestudio.net>
2022-09-30 19:41:00 +0200

    fix helmet

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
-
-document.title = "RageStudio"
+import { Helmet } from "react-helmet"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(
   <React.StrictMode>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>RageStudio</title>
+      <meta
+        name="description"
+        content="Official RageStudio webpage."
+      />
+    </Helmet>
     <App />
   </React.StrictMode>
 )
